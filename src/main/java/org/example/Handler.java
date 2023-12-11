4e5af8a7753342f144873755d8d25ce1792ec13d
rory-hayes1 <rory@frankieone.com>
2023-12-12 09:55:16 +1100

    Minor change
    Minor updates, removing redundant method

--- src/main/java/org/example/Handler.java
+++ src/main/java/org/example/Handler.java
@@ -1,13 +1,12 @@
 package org.example;
 
-import software.amazon.awssdk.core.ResponseInputStream;
+
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.textract.TextractClient;
 import software.amazon.awssdk.services.textract.model.*;
-
-import java.io.ByteArrayInputStream;
+import software.amazon.awssdk.services.textract.model.DocumentLocation;
 
 public class Handler {
     private final S3Client s3Client;
@@ -25,8 +24,7 @@ public class Handler {
         createBucket(s3Client, bucket);
 
         System.out.println("Uploading object...");
-        s3Client.putObject(PutObjectRequest.builder().bucket(bucket).key(key)
-                        .build(),
+        s3Client.putObject(PutObjectRequest.builder().bucket(bucket).key(key).build(),
                 RequestBody.fromString("Testing with the {sdk-java}"));
 
         System.out.println("Upload complete");
@@ -36,7 +34,7 @@ public class Handler {
 
         cleanUp(s3Client, bucket, key);
 
-        System.out.println("Closing the connection to {S3} and Textract");
+        System.out.println("Closing the connection to S3 and Textract");
         s3Client.close();
         textractClient.close();
         System.out.println("Connections closed");
@@ -51,35 +49,57 @@ public class Handler {
             System.out.println(bucketName + " is ready.");
             System.out.printf("%n");
         } catch (S3Exception e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
+            System.err.println("Error creating bucket: " + e.awsErrorDetails().errorMessage());
+            // Handle the exception or throw it instead of exiting
         }
     }
 
     public static void analyzeDocument(TextractClient textractClient, String bucketName, String keyName) {
         System.out.println("Analyzing document...");
         try {
-            S3Object s3Object = S3Object.builder().bucket(bucketName).name(keyName).build();
-            Document document = Document.builder().s3Object(s3Object).build();
+            software.amazon.awssdk.services.textract.model.S3Object s3ObjectTextract =
+                    software.amazon.awssdk.services.textract.model.S3Object.builder()
+                            .bucket(bucketName)
+                            .name(keyName)
+                            .build();
+
+            Document document = Document.builder().s3Object(s3ObjectTextract).build();
+
+            DocumentLocation documentLocation = DocumentLocation.builder()
+                    .s3Object(s3ObjectTextract)
+                    .build();
 
             StartDocumentTextDetectionResponse startResponse = textractClient.startDocumentTextDetection(
                     StartDocumentTextDetectionRequest.builder()
-                            .documentLocation(document)
+                            .documentLocation(documentLocation)
                             .build());
 
             String jobId = startResponse.jobId();
 
-            // Wait for the document analysis job to complete
-            textractClient.waiter().waitUntilDocumentTextDetectionComplete(
-                    DescribeDocumentTextDetectionRequest.builder().jobId(jobId).build());
+            boolean jobComplete = false;
+            do {
+                GetDocumentTextDetectionResponse response = textractClient.getDocumentTextDetection(
+                        GetDocumentTextDetectionRequest.builder().jobId(jobId).build());
 
-            // Retrieve the results
-            GetDocumentTextDetectionResponse response = textractClient.getDocumentTextDetection(
-                    GetDocumentTextDetectionRequest.builder().jobId(jobId).build());
+                JobStatus jobStatus = response.jobStatus();
+                System.out.println("Job Status: " + jobStatus);
+
+                if (jobStatus == JobStatus.SUCCEEDED || jobStatus == JobStatus.FAILED) {
+                    jobComplete = true;
+                } else {
+                    // Sleep for a short duration before polling again
+                    try {
+                        Thread.sleep(5000); // Adjust the sleep duration as needed
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            } while (!jobComplete);
 
             // Process the results as needed
             System.out.println("Document analysis complete. Results:");
-            System.out.println(response.blocks());
+            System.out.println(textractClient.getDocumentTextDetection(
+                    GetDocumentTextDetectionRequest.builder().jobId(jobId).build()).blocks());
 
             System.out.printf("%n");
         } catch (TextractException | S3Exception e) {
