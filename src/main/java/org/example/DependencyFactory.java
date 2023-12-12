e807cf300820c5b32a6bfa7cd981bbb69ba81165
Edward Yakop <edward@frankieone.com>
2023-12-12 08:30:07 +0800

    Add test to run pdf

--- src/main/java/org/example/DependencyFactory.java
+++ src/main/java/org/example/DependencyFactory.java
@@ -1,22 +1,33 @@
 
 package org.example;
 
-import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 /**
  * The module containing all dependencies required by the {@link Handler}.
  */
 public class DependencyFactory {
+    private static final Region region = Region.US_EAST_1;
+    private static final String accessKey = "your-access-key";
+    private static final String secret = "your-secret-key";
 
-    private DependencyFactory() {}
+    private DependencyFactory() {
+    }
+
+    static AwsBasicCredentials credentials() {
+        return AwsBasicCredentials.create(accessKey, secret);
+    }
 
     /**
      * @return an instance of S3Client
      */
     public static S3Client s3Client() {
         return S3Client.builder()
-                .httpClientBuilder(ApacheHttpClient.builder())
+                .region(region) // replace with your desired region
+                .credentialsProvider(StaticCredentialsProvider.create(credentials()))
                 .build();
     }
 }
